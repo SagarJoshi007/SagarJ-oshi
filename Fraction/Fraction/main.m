a4c8fd74e2d8cd7f014a446560feb32d782b5234
SinuSundaresan <sinusundaresan@yahoo.com>
2014-05-14 18:11:23 -0400

    Initial Commit

---
+++ Fraction/Fraction/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by User1 on 2014-05-14.
+//  Copyright (c) 2014 a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
